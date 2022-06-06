512d494d7672fb042a325b8cce9258b01bdb1c74
AlaminSarkerFRII <alamin.education1997@gmail.com>
2022-06-06 20:55:00 +0600

    decleared function

--- PyGame.py
+++ PyGame.py
@@ -1,14 +1,32 @@
 
+
 from tkinter import messagebox , Tk
 import pygame,sys,random
 
 
-window_width,window_height = 500,500
-window = pygame.display.set_mode((window_width,window_height))
+width,height = 400,400
+game_screen = pygame.display.set_mode((width,height))
 
 clock = pygame.time.Clock()
 
-def main() : 
+x, y = 200,200
+delta_x , delta_y = 0, 0
+
+def snack() : 
+    global x , y
+    x = x + delta_x
+    y = y + delta_y
+    pygame.draw.rect(game_screen,(255,255,255),[x,y,10,10])
+    pygame.display.update()
+
+
+def main() :
+    
+    # Snacks
+    
+   
+    
+     
     run = True
     while run :
         # Quit 
@@ -16,13 +34,17 @@ def main() :
             if event.type== pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+                snack()
+                # Snack Movements 
+             
+                
+                # Game Over 
+              
                 
                 # Draw 
-                
                 window.fill((0, 0, 0))
                 
                 # Update 
-                
                 pygame.display.flip()
                 clock.tick(8)
                 
